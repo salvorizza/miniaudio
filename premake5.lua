f9f7d069b2c8a6cb1414499c0c952d4903f2f3bd
Rizza Salvatore <salvatore.rizza@cgm.com>
2024-05-14 18:40:02 +0200

    Added premake

---
+++ premake5.lua
@@ -0,0 +1,24 @@
+project "miniaudio"
+    kind "StaticLib"
+    language "C"
+    staticruntime "off"
+    
+    targetdir ("bin/" .. outputdir .. "/%{prj.name}")
+    objdir ("bin-int/" .. outputdir .. "/%{prj.name}")
+
+    files
+    {
+        "miniaudio.h",
+        "miniaudio.c"
+    }
+    
+    filter "system:windows"
+        systemversion "latest"
+
+    filter "configurations:Debug"
+        runtime "Debug"
+        symbols "on"
+
+    filter "configurations:Release"
+        runtime "Release"
+        optimize "on"
